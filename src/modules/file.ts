b44940348e78f84133228ae81a8501b12aca94dc
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2020-12-27 21:39:14 +0800

    feat(file): write file support

--- src/modules/file.ts
+++ src/modules/file.ts
@@ -1,7 +1,22 @@
-import { existsSync, readFile } from "fs";
+import { existsSync, readFile, writeFile } from "fs";
 import { registryFunc } from "../operation";
 function reg() {
   registryFunc("readFile", mReadFile);
+  registryFunc("writeFile", mWriteFile);
+}
+function mWriteFile(data: any, cb: (state: boolean, res: any) => void) {
+  if (data["path"] == undefined || data["data"] == undefined) {
+    cb(false, "Missing path or data");
+    return;
+  } else {
+    writeFile(data["path"], data["data"], (e) => {
+      if (e) {
+        cb(false, e);
+      } else {
+        cb(true, null);
+      }
+    });
+  }
 }
 function mReadFile(data: any, cb: (state: boolean, res: any) => void) {
   if (data["path"] == undefined) {
